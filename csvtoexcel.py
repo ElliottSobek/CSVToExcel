c8d5e62b1b30720fc7af6c1bcbfe4959bb0a3df3
Elliott <esobek@bbex.com>
2018-03-17 16:40:36 -0600

    Add license header

--- csvtoexcel.py
+++ csvtoexcel.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+#     CSV To Excel; Converts a CSV file to an Excel file
+#     Copyright (C) 2018  Elliott Sobek
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
